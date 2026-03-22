ff3e7ef1672b2c09f34bb0f9845d4dac4161cf3f
Ivanchux <ivanchu8888@gmail.com>
2026-03-22 21:23:28 +0100

    Fase 1 - Texturizado por altura: nieve, roca, hierba, agua

--- static_body_3d.gd
+++ static_body_3d.gd
@@ -17,20 +17,33 @@ func _ready():
 		for x in resolution:
 			heights.append(noise.get_noise_2d(x, z) * 10.0)
 	
-	# Colision con HeightMapShape3D
+	# Colision
 	var hmap = HeightMapShape3D.new()
 	hmap.map_width = resolution
 	hmap.map_depth = resolution
 	hmap.map_data = heights
 	collision.shape = hmap
 	
-	# Malla visual
+	# Malla visual con color por altura
 	var surface = SurfaceTool.new()
 	surface.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
 	for z in resolution:
 		for x in resolution:
 			var height = noise.get_noise_2d(x, z) * 10.0
+			
+			# Color segun altura
+			var color
+			if height > 6.0:
+				color = Color(0.9, 0.9, 0.95) # nieve
+			elif height > 3.0:
+				color = Color(0.5, 0.45, 0.4) # roca
+			elif height > 0.0:
+				color = Color(0.2, 0.55, 0.2) # hierba
+			else:
+				color = Color(0.15, 0.35, 0.6) # agua
+			
+			surface.set_color(color)
 			surface.set_uv(Vector2(float(x)/resolution, float(z)/resolution))
 			surface.add_vertex(Vector3(
 				(x - resolution/2.0) * (float(size)/resolution),
@@ -51,6 +64,23 @@ func _ready():
 	surface.generate_normals()
 	mesh_instance.mesh = surface.commit()
 	
+	# Material que usa el color de vertices
 	var mat = StandardMaterial3D.new()
-	mat.albedo_color = Color(0.2, 0.6, 0.2)
+	mat.vertex_color_use_as_albedo = true
 	mesh_instance.material_override = mat
+	
+	# Cielo
+	var sky_material = ProceduralSkyMaterial.new()
+	sky_material.sky_top_color = Color(0.05, 0.05, 0.2)
+	sky_material.sky_horizon_color = Color(0.3, 0.4, 0.6)
+	sky_material.ground_horizon_color = Color(0.3, 0.4, 0.6)
+	
+	var sky = Sky.new()
+	sky.sky_material = sky_material
+	
+	var env = Environment.new()
+	env.background_mode = Environment.BG_SKY
+	env.sky = sky
+	
+	var world_env = get_parent().get_node("WorldEnvironment")
+	world_env.environment = env
